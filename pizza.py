06f1c0f1c7babb06e973f1f5781f68f932341b6a
LiuvaSalas <liuvasalas01@gmail.com>
2024-08-23 18:59:29 -0400

    primer commit: Se generaron los archivos corrrespondientes al desafio con los modulos y clases necesarias para ejecutar el proyecto

---
+++ pizza.py
@@ -0,0 +1,46 @@
+from ingredientes import vegetales, masa, proteinas
+
+class Pizza:
+    precio = 10000
+    tamanio = "Familiar"
+    
+    
+    def __init__(self, i_proteico=None, vegetal_1=None, vegetal_2=None, t_masa=None):
+        self.i_proteico = i_proteico
+        self.vegetal_1 = vegetal_1
+        self.vegetal_2 = vegetal_2
+        self.t_masa = t_masa
+        self.pizza_valida = None  # Se calculará después
+        
+        
+    @staticmethod
+    def casos_posibles(evaluacion,ingredientes: list):
+        
+        if evaluacion in ingredientes:
+            # print("Lo logre")
+            return True
+        # print("Hay un error en su pedido")
+        return False
+    
+    
+    def realizar_pedido(self):
+        self.i_proteico = input("Por favor ingrese el tipo de proteína (pollo, uno, carne vegevactal): ")
+        self.vegetal_1 =  input("Por favor ingrese el primer ingrediente vegetal (tomates, aceitunas, champiñones): ")
+        self.vegetal_2 =  input("Por favor ingrese el segundo ingrediente vegetal (tomates, aceitunas, champiñones): ")
+        self.t_masa =  input("Por favor ingrese el tipo de masa (tradicional, delgada): ")
+        
+        v_1 = Pizza.casos_posibles(self.i_proteico, proteinas)
+        v_2 = Pizza.casos_posibles(self.vegetal_1,vegetales)
+        v_3 = Pizza.casos_posibles(self.vegetal_2, vegetales)
+        v_4 = Pizza.casos_posibles(self.t_masa, masa)
+
+        lista_respuesta = [v_1, v_2, v_3, v_4]
+
+        if False in lista_respuesta:
+            self.pizza_valida = "Su pedido tiene un error. Por favor verifique los ingredientes y vuelva a intentarlo."
+            
+        else:
+            self.pizza_valida = "¡Su pedido está correcto! Gracias por su compra."
+            
+            #print(f"Resumen del pedido: \nProteína: {self.i_proteico}\nVegetales: {self.vegetal_1}, {self.vegetal_2}\nMasa: {self.t_masa}\nPrecio total: {Pizza.precio} pesos.")
+    
